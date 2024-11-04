0d4d9b866625ac2855e3f639b37201e756381ddd
MrBarker47 <jebarker7@gmail.com>
2024-11-04 13:22:59 -0500

    Update the files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //Variables
 let header = document.createElement("h1");
 let text = document.createElement("input");
+let button = document.createElement("button");
 
-//DOM - header - text 
-header.textContent = "Hello World";
+//DOM - header - text - button
+header.textContent = "NFL/College Status";
+header.style.marginLeft = "520px";
+header.style.marginTop = "50px";
 document.body.appendChild(header);
+
+text.style.marginLeft = "570px";
+text.style.marginTop = "5px";
+document.body.appendChild(text);
+
+button.textContent = "Search";
+button.style.marginLeft = "20px";
+button.type = "submit";
+document.body.append(button);
